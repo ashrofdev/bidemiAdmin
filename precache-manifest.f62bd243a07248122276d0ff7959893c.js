d12bbecab7cd0b4f8eb90becf270c8b252ac3af8
First Last <salmanashraf853@gmail.com>
2020-03-06 12:49:42 +0100

    Updates

---
+++ precache-manifest.f62bd243a07248122276d0ff7959893c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0673e76a80b7c46f65f86cd0a88542f7",
+    "url": "/bidemiAdmin/index.html"
+  },
+  {
+    "revision": "d71fa45e1751cb51e507",
+    "url": "/bidemiAdmin/static/css/main.38cb5487.chunk.css"
+  },
+  {
+    "revision": "720d74c1191a11cd40ec",
+    "url": "/bidemiAdmin/static/js/2.bdd5e318.chunk.js"
+  },
+  {
+    "revision": "d71fa45e1751cb51e507",
+    "url": "/bidemiAdmin/static/js/main.c607fd20.chunk.js"
+  },
+  {
+    "revision": "c86902dac630ad0d1426",
+    "url": "/bidemiAdmin/static/js/runtime-main.0f3e0c9f.js"
+  },
+  {
+    "revision": "200356c3d8b994dab3d8a539b4764c1e",
+    "url": "/bidemiAdmin/static/media/1.200356c3.jpg"
+  },
+  {
+    "revision": "b85d07cafa9fca7824d637a59ea78159",
+    "url": "/bidemiAdmin/static/media/3.b85d07ca.jpg"
+  },
+  {
+    "revision": "cffa9d9d88890cfcd0ae3d6dd7a6efa9",
+    "url": "/bidemiAdmin/static/media/3.cffa9d9d.gif"
+  },
+  {
+    "revision": "efd6830c763671e0c6893836b2a9dd7d",
+    "url": "/bidemiAdmin/static/media/mac.efd6830c.jpg"
+  }
+]);
